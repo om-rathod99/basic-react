16e5f811fad8ef26150d163e7a477df8a8a0a090
om-rathod99 <om.rathod@cincologic.com>
2024-09-04 15:19:37 +0530

    counter project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,11 @@ import Maggie from "./maggie"
 
 function App() {
 
-
+  const username = "maggie"
   return (
     <>
     <Maggie/>
-    <h1>Basic react</h1>
+    <h1>Basic react {username}</h1>
     <p>test para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | Maggie</h1>
+        </div>
+    )
+}
 
-createRoot(document.getElementById('root')).render(
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https://www.google.com",
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
 
-    <App />
+const anotherElement = (
+    <a href="https://google.com" target = '_blank'>Visit Google</a>
+)
+
+const anotherUser = "maggie and react"
+
+const reactElement = React.createElement(
+    'a', 
+    {href:'https://google.com', target:'_blank'},
+    'Click me to Visit GooGle',
+    anotherElement
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    reactElement
   
 )
